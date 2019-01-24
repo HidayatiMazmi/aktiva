7c2108aa56c685d4cedaed1cb6adc75de9eb23b7
HidayatiMazmi <HidayatiMazmi@users.noreply.github.com>
2019-01-24 12:43:30 +0700

    update database

--- aktiva.sql
+++ aktiva.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jan 2019 pada 05.08
+-- Waktu pembuatan: 24 Jan 2019 pada 06.42
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -31,6 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `aset` (
   `id` int(11) UNSIGNED NOT NULL,
   `nama_aset` varchar(1000) NOT NULL,
+  `kode_aset` varchar(20) NOT NULL,
   `tgl_terima` date NOT NULL,
   `masa_pemakaian` varchar(50) NOT NULL,
   `foto_fisik_aset` varchar(1000) NOT NULL,
@@ -42,6 +43,37 @@ CREATE TABLE `aset` (
   `id_kategori` int(11) UNSIGNED NOT NULL,
   `id_lokasi` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `aset`
+--
+
+INSERT INTO `aset` (`id`, `nama_aset`, `kode_aset`, `tgl_terima`, `masa_pemakaian`, `foto_fisik_aset`, `kondisi_awal`, `nilai_aset`, `keterangan`, `id_user`, `id_jenis`, `id_kategori`, `id_lokasi`) VALUES
+(1, 'komputer', '', '2018-05-03', '2 tahun', '', 'Baik', 4000000, '', 1, 1, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hari`
+--
+
+CREATE TABLE `hari` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama_hari` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `hari`
+--
+
+INSERT INTO `hari` (`id`, `nama_hari`) VALUES
+(1, 'Senin'),
+(2, 'Selasa'),
+(3, 'Rabu'),
+(4, 'Kamis'),
+(5, 'Jum\'at'),
+(6, 'Sabtu'),
+(7, 'Minggu');
 
 -- --------------------------------------------------------
 
@@ -59,8 +91,8 @@ CREATE TABLE `jenis_asset` (
 --
 
 INSERT INTO `jenis_asset` (`id`, `nama_jenis`) VALUES
-(1, '100%'),
-(2, '92.5% & 7.5%');
+(1, 'Aset Bergerak'),
+(2, 'Aset Tidak Bergerak');
 
 -- --------------------------------------------------------
 
@@ -73,6 +105,14 @@ CREATE TABLE `kategori` (
   `nama_kategori` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama_kategori`) VALUES
+(1, 'Perangkat Elektronik'),
+(2, 'Tanah');
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +123,35 @@ CREATE TABLE `lokasi` (
   `id` int(11) UNSIGNED NOT NULL,
   `nama_lokasi` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `lokasi`
+--
+
+INSERT INTO `lokasi` (`id`, `nama_lokasi`) VALUES
+(1, 'Blitar'),
+(2, 'Malang');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemeliharaan`
+--
+
+CREATE TABLE `pemeliharaan` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `hasil_pemeliharaan` varchar(20) NOT NULL,
+  `id_aset` int(11) UNSIGNED NOT NULL,
+  `tanggal_pemeliharaan` date NOT NULL,
+  `id_hari` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pemeliharaan`
+--
+
+INSERT INTO `pemeliharaan` (`id`, `hasil_pemeliharaan`, `id_aset`, `tanggal_pemeliharaan`, `id_hari`) VALUES
+(1, 'Baik', 1, '2019-01-10', 1);
 
 -- --------------------------------------------------------
 
@@ -96,16 +165,17 @@ CREATE TABLE `user` (
   `password` varchar(1000) NOT NULL,
   `nama` varchar(1000) NOT NULL,
   `nip` varchar(1000) NOT NULL,
-  `role` varchar(1000) NOT NULL
+  `role` varchar(1000) NOT NULL,
+  `photo` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `nama`, `nip`, `role`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '1', 'Admin'),
-(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', '2', 'User');
+INSERT INTO `user` (`id`, `username`, `password`, `nama`, `nip`, `role`, `photo`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '1', 'Admin', ''),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', '2', 'User', '');
 
 --
 -- Indexes for dumped tables
@@ -120,6 +190,12 @@ ALTER TABLE `aset`
   ADD KEY `id_kategori` (`id_jenis`),
   ADD KEY `id_kategori_2` (`id_kategori`),
   ADD KEY `id_lokasi` (`id_lokasi`);
+
+--
+-- Indeks untuk tabel `hari`
+--
+ALTER TABLE `hari`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `jenis_asset`
@@ -140,6 +216,14 @@ ALTER TABLE `lokasi`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `pemeliharaan`
+--
+ALTER TABLE `pemeliharaan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_aset` (`id_aset`),
+  ADD KEY `id_hari` (`id_hari`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -153,7 +237,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `aset`
 --
 ALTER TABLE `aset`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `hari`
+--
+ALTER TABLE `hari`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `jenis_asset`
@@ -165,13 +255,19 @@ ALTER TABLE `jenis_asset`
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `lokasi`
 --
 ALTER TABLE `lokasi`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemeliharaan`
+--
+ALTER TABLE `pemeliharaan`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
@@ -191,6 +287,13 @@ ALTER TABLE `aset`
   ADD CONSTRAINT `aset_ibfk_2` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_asset` (`id`),
   ADD CONSTRAINT `aset_ibfk_3` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi` (`id`),
   ADD CONSTRAINT `aset_ibfk_4` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `pemeliharaan`
+--
+ALTER TABLE `pemeliharaan`
+  ADD CONSTRAINT `pemeliharaan_ibfk_1` FOREIGN KEY (`id_hari`) REFERENCES `hari` (`id`),
+  ADD CONSTRAINT `pemeliharaan_ibfk_2` FOREIGN KEY (`id_aset`) REFERENCES `aset` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
